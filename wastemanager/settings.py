9313191b081ea31fc176ae4b27cdcb50a70b71ee
islem <im_karabernou@esi.dz>
2021-12-18 02:00:15 +0100

    Automatic configuration with django-heroku

--- wastemanager/settings.py
+++ wastemanager/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-a(qhn55)kxy+5ncrgydamvh5f8y-nnio8qwu-6@z9h$0k%yzxo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
